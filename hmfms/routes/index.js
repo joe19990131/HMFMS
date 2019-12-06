b773e3a581e92e43f4199bf9ceb5405ef9a7d7f1
joe19990131 <32476180+joe19990131@users.noreply.github.com>
2019-12-07 01:07:22 +0800

    path bug fixed

--- hmfms/routes/index.js
+++ hmfms/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'HMFMS' });
+  res.render('index');
 });
 router.get(/(.*)\.(jpg|gif|png|ico|css|js|txt|svg|ttf|eot|woff)/i, function(req, res) {
   res.sendfile(__dirname + "/" + req.params[0] + "." + req.params[1], function(err) {

e4f3723f72b3b5d3e2c63b409b3da4d6abcbc7be
joe19990131 <32476180+joe19990131@users.noreply.github.com>
2019-12-07 18:39:49 +0800

    selecter page reuter fixed

--- hmfms/app.js
+++ hmfms/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var locationSelecterRouter = require('./routes/locationSelecter');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, '/kendo')));
 //app.use(express.static(path.join(__dirname, '/public/stylesheets')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/locationSelecter', locationSelecterRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- hmfms/routes/locationSelecter.js
+++ hmfms/routes/locationSelecter.js
@@ -1,18 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 router.get('/locationSelecter', function(req, res, next) {
-  res.render('locationSelecter');
+  res.send('respond with a locationSelecter');
 });
 router.get(/(.*)\.(jpg|gif|png|ico|css|js|txt|svg|ttf|eot|woff)/i, function(req, res) {
   res.sendfile(__dirname + "/" + req.params[0] + "." + req.params[1], function(err) {
       if (err) res.send(404);
   });
 });
-
-
 module.exports = router;

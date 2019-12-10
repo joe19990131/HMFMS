a26872c0b3ffa7c1bd25700aca949bd0f2f1ecdd
joe19990131 <joe21854946@gmail.com>
2019-12-10 17:22:22 +0800

    can send json file to router

--- hmfms/app.js
+++ hmfms/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var bodyParser = require('body-parser');
 //var conn  = require ('./lib/db');
 
 var indexRouter = require('./routes/index');

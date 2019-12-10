430a404f87923bc360e01fd5582821e7104d18a0
joe19990131 <joe21854946@gmail.com>
2019-12-10 13:47:18 +0800

    需要研究如何用kendoUI去送資料

--- hmfms/app.js
+++ hmfms/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var conn  = require ('./lib/db');
+//var conn  = require ('./lib/db');
 
 var indexRouter = require('./routes/index');
 var locationSelecterRouter = require('./routes/locationSelecter');
--- hmfms/routes/index.js
+++ hmfms/routes/index.js
@@ -27,15 +27,17 @@ router.get('/newLocation', function(req, res, next) {
   res.render('newLocation');
 });
 //form send file test
-router.post('/searchByEq',function(req, res, next) {
+router.post('/index/searchByEq',function(req, res, next) {
+  console.log("HERE!!!");
+  //console.log(req.route);
   var oid = req.body.OID;
   var ot = req.body.OT;
   var ts = req.body.TS;
   var cs = req.body.CS;
   var exp = req.body.EXP;
-  conn.connect();
-
-  res.send(oid+" "+ot+" "+ts+" "+cs+" "+exp);
+  //conn.connect();
+  console.log(oid+" "+ot+" "+ts+" "+cs+" "+exp);
+  //res.send(oid+" "+ot+" "+ts+" "+cs+" "+exp);
 });
 router.get(/(.*)\.(jpg|gif|png|ico|css|js|txt|svg|ttf|eot|woff)/i, function(req, res) {
   res.sendfile(__dirname + "/" + req.params[0] + "." + req.params[1], function(err) {

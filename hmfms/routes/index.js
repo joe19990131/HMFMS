b77abe570427f605ca117ce4fd796ac27e66829e
joe19990131 <joe21854946@gmail.com>
2019-12-11 20:08:13 +0800

    search by Loc post connect done

--- hmfms/routes/index.js
+++ hmfms/routes/index.js
@@ -48,6 +48,16 @@ router.post('/searchByEq',function(req, res, next) {
 });
 
 
+router.post('/searchByLoc',function(req,res,next){
+  var locId = req.body.LocID;
+  var HID = req.body.HID;
+  var BID = req.body.BID;
+  var LocFloor = req.body.LocFloor;
+  var LocName = req.body.LocName;
+
+console.log(locId+' '+HID+' '+BID+' '+LocFloor+' '+LocName);
+});
+
 router.post('/searchByLoc',function(req, res, next) {
   console.log("HERE!!!");
 

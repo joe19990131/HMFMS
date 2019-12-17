5d4bc9050a5b1c9183f0bdad651addfccc24f0d7
joe19990131 <32476180+joe19990131@users.noreply.github.com>
2019-12-18 00:32:20 +0800

    conn bug fix
    解決重複連接問題

--- hmfms/routes/index.js
+++ hmfms/routes/index.js
@@ -18,10 +18,12 @@ var conn = mysql.createConnection({
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.location('/');
   res.render('index');
 });
 router.get('/locationSelecter', function(req, res, next) {
-  res.render('locationSelecter');
+  res.redirect(301,'../locationSelecter');
+  //res.render('');
 });
 router.get('/newEquitment', function(req, res, next) {
   res.render('newEquitment');
@@ -33,13 +35,14 @@ router.get('/objReq', function(req, res, next) {
   res.render('objReq');
 });
 //form send file test
+var connStatus = 0;
 router.post('/searchByEq',function(req, res, next) {
   var oid = req.body['OID'];
   var ot = req.body['ObjectType'];
   var ts = req.body['TypeSpec'];
   var cs = req.body['checkStituation'];
   var isNE = req.body['isNE'];
-  console.log("HERE!!!");
+  console.log("HERE IS SBE");
   console.log(req.body.OID);
   console.log(isNE);
   console.log(req.body);
@@ -47,10 +50,17 @@ router.post('/searchByEq',function(req, res, next) {
   console.log(oid+" "+ot+" "+ts+" "+cs+" ");
 
 var sql1 = "select oid as OID,objtype as ObjectType,TypeSpec,checkstituation as checkStituation,checkDate,ManufatureDate,EXPdate,location_info.LocName,(select location_info.LocName from obj_info join location_info on obj_info.MigrateLocID = location_info.LocID) as MigrateLocID from obj_info join location_info using(LocID) where oid = 10 order by OID;";
-conn.connect(function(err){
-  if(err) throw err;
-  console.log('connect success!');
-  });
+
+
+console.log(connStatus);
+if(connStatus == 0){
+  conn.connect(function(err){
+    if(err) throw err;
+    console.log('connect success!');
+    connStatus ++;
+    console.log(connStatus);
+    });
+}
 
 conn.query(sql1,function(err,rows){
     console.log(rows);
@@ -58,16 +68,20 @@ conn.query(sql1,function(err,rows){
       console.log(err);
     }else{
       res.json(rows);
+      //res.end();
     }
   }
   
   );
+  console.log(connStatus);
+  /*if(connStatus == 1){
   conn.end(function(err){
     if(err) throw err;
     console.log('connect end');
     });
-
-
+    connStatus = 0;
+}*/
+  
 //res.json({OID:1,ObjectType:"乾粉",TtpeSpec:"5",checkStituation:"正常",CheckDate:"2019/01/22",ManufatureDate:"2010/01/22",EXPdate:"2020/01/22",Location:"703-1",MigrateLoc:""});
 });
 

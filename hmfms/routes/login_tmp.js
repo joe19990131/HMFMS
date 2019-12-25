b377f735f6cd6a296383642a975729e6e7824708
DD0s-JCN13 <jimmylin160@gmail.com>
2019-12-25 18:48:48 +0800

    login js science temp #2

--- hmfms/routes/login_tmp.js
+++ hmfms/routes/login_tmp.js
@@ -2,7 +2,15 @@ var express = require('express');
 var mysql = require('mysql');
 var router = express.Router();
 var bodyParser = require('body-parser');
+var session = require('express-session');
 var app = express();
+
+//setting part "webpage file readin from server"
+router.get(/(.*)\.(jpg|gif|png|ico|css|js|txt|svg|ttf|eot|woff)/i, function(req, res) {
+    res.sendfile(__dirname + "/" + req.params[0] + "." + req.params[1], function(err) {
+        if (err) res.send(404);
+    });
+  });
 
 //body-parser bulit-up
 app.use(bodyParser).json({limit: '1mb'});
@@ -19,25 +27,48 @@ var conn = mysql.createConnection({
     database: 'hmfmsdatabase'
 });
 
+//get login, index page
+router.get('/', function(req, res, next){
+    res.render('login');
+});
+router.get('/', function(req, res, next){
+    res.render('index');
+});
 
 //passport setting area
 var passport = require('passport');
 var Strtegy = require('passport').Strategy;
+router.post('/login', function(req, res) {
+    var Uname = req.body.username;
+    var password = req.body.password;
+    var sql = 'select uid, password from member_m where (uid = "'+ Uname +'" and password = "'+ password +'")';
 
-var Uname = req.body.username;
-var password = req.body.password;
-var sql = 'select uid, password from member_m where (uid = "'+ Uname +'" and password = "'+ password +'")';
-
-passport.use(new Strtegy(
-    //verify callback func.
-    function(Uname, password, done){
-        //if null
-        if(err){return done(err);}
-        
-        //if user not exist
-        if(!user){
-            return done(null, false,)
+    passport.use(new Strtegy(
+        //verify callback func.
+        function(Uname, password){
+            if(Uname && password){
+                conn.query(sql,[Uname, password],function(error, result,fields){
+                    if(result.lenght >0){
+                        res.redirect('/index');
+                    }else{
+                        res.send("帳號或密碼錯誤");
+                    }
+                })
+            }else{
+                res.send("請輸入帳號密碼");
+            }
+            console.log(Uname+' '+password);
         }
-    }
-));
+    ));
+});
 
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(session({
+    secret: 'Science_local',
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+module.exports = router;
